925ac8324349930cf8228872e0e1d84ebb74c34d
Titli Basu <titlibasu6790@gmail.com>
2026-05-22 16:41:44 +0530

    Update changes on main branch

--- src/api/products.py
+++ src/api/products.py
@@ -3,6 +3,6 @@ products = []
 def get_products():
     return products
 
-def add_product(name, price):
+def add_product(name, price)
     products.append({"food": name, "price": price})
     return {"added": True}
